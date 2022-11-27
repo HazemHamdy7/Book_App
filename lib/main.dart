9003963d9ad6e9e93aee75a32c74834eb73a34e1
HazemHamdy7 <hazemhamdy1215@gmail.com>
2022-11-27 22:12:05 +0200

    create splash view body

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'spilash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +13,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: (Column()));
+    return const GetMaterialApp(
+      home: SpilashScereen(),
+    );
   }
 }
---
+++ lib/spilash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'Features/Spilash/presention/views/widgets/splash_view_body.dart';
+
+class SpilashScereen extends StatelessWidget {
+  const SpilashScereen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
