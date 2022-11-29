e361aae80c3a67856eb2b9f72dd9850d7d53e169
HazemHamdy7 <hazemhamdy1215@gmail.com>
2022-11-29 20:45:59 +0200

    Add Animation to Text in Spilash Screen

--- lib/Features/Spilash/presention/views/widgets/splash_view_body.dart
+++ lib/Features/Spilash/presention/views/widgets/splash_view_body.dart
@@ -1,8 +1,38 @@
 import 'package:book_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashViewBody extends StatelessWidget {
+import 'sliding_text.dart';
+
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(minutes: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +42,7 @@ class SplashViewBody extends StatelessWidget {
       children: [
         Image.asset(AssetsData.loge),
         const SizedBox(height: 10),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
