8bf2c30b05439c6d919717ab75422679fefca571
HazemHamdy7 <hazemhamdy1215@gmail.com>
2022-12-08 07:17:01 +0200

    best seller list view item part one

--- lib/Features/home/presentation/views/widgets/home_view_bady.dart
+++ lib/Features/home/presentation/views/widgets/home_view_bady.dart
@@ -1,4 +1,5 @@
 import 'package:book_app/Features/home/presentation/views/widgets/best_saler.dart';
+import 'package:book_app/core/utils/assets.dart';
 import 'package:book_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -24,7 +25,60 @@ class HomeViewBady extends StatelessWidget {
             "Best Seller",
             style: StylesApp.titleMedium,
           ),
+          BestSellerListViewItem(),
         ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+      child: SizedBox(
+        height: 125,
+        child: Row(
+          children: [
+            // to responsive image widget
+            AspectRatio(
+              aspectRatio: 2.5 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: Colors.red,
+                    image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        AssetsData.test2,
+                      ),
+                    )),
+              ),
+            ),
+            // Column(
+            //   children: [
+            //     Text(
+            //       "Best Seller",
+            //       style: StylesApp.titleMedium,
+            //     ),
+            //     SizedBox(
+            //       height: 30,
+            //     ),
+            //     Row(
+            //       children: [
+            //         Text(
+            //           "19.19\$",
+            //           style: StylesApp.titleMedium,
+            //         ),
+            //       ],
+            //     ),
+            //   ],
+            // )
+          ],
+        ),
       ),
     );
   }
