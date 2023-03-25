c5d40a29fb3f10a69bffb51264155265513bcc79
HazemHamdy7 <hazemhamdy1215@gmail.com>
2023-03-25 14:41:51 +0200

    create featured books methed

--- lib/Features/home/presentation/manger/featured books cubit/featured_books_cubit.dart
+++ lib/Features/home/presentation/manger/featured books cubit/featured_books_cubit.dart
@@ -2,8 +2,23 @@ import 'package:bloc/bloc.dart';
 import 'package:book_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../data/repos/home_repo.dart';
+
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeatureBooks() async {
+    emit(FeaturedBooksLoading());
+
+    var result = await homeRepo.fetchFeaturedBook();
+    result.fold(
+        (failure) => {
+              emit(FeaturedBooksFailure(failure.errorMessage)),
+            }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
