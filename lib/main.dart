1067249cd66873c2cec82492178d4e3618b12009
HazemHamdy7 <hazemhamdy1215@gmail.com>
2023-04-01 15:20:28 +0200

    display newset list view data

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {      
+  Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider(
@@ -26,8 +26,8 @@ class MyApp extends StatelessWidget {
                   getIt.get<HomeRepoImpl>(),
                 )..fetchFeatureBooks()),
         BlocProvider(
-            create: (context) => NewsetBooksCubit(
-                  getIt.get<HomeRepoImpl>(),
+          create: (context) => NewsetBooksCubit(
+            getIt.get<HomeRepoImpl>(),
                 )),
       ],
       child: MaterialApp.router(
